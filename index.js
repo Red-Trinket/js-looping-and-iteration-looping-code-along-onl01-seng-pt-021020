d57a7a3c78e19350593ba673c16215c7b618edc7
Matthew Jones <matts3371@gmail.com>
2020-08-17 13:06:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(names){
+    for(let i = 0; i < names.length; i++){
+    names[i] = 'Thank you, ' + names[i] + ', for the wonderful surprise gift!';
+}
+return names;
+}
+
+function countDown(number){
+    for(let i = number; i >= 0; i--){
+        console.log(i);
+    }
+}
